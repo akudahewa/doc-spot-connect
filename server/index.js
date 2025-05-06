6191ae389e2f31f39b321a9768365c4c967bfe1b
Amila <amila@libt.co.uk>
2025-05-06 05:34:29 +0530

    add interceptor

--- server/index.js
+++ server/index.js
@@ -20,6 +20,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/doctor-reservation')
